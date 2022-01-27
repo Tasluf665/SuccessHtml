06cb8d8e8dd06a7a67f58e4a8808a1b970ca9f93
Tasluf665 <tasluf15-12089@diu.edu.bd>
2022-01-27 22:37:07 +0600

    Done

---
+++ script.js
@@ -0,0 +1,7 @@
+var animation = bodymovin.loadAnimation({
+  container: document.getElementById("animContainer"),
+  renderer: "svg",
+  loop: false,
+  autoplay: true,
+  path: "/assect/success.json", // lottie file path
+});
